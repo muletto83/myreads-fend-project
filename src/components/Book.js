5e0b4d2f926d7892fb46adae41635252afb3d277
muletto83 <muletto83@users.noreply.github.com>
2018-11-24 06:17:54 -0500

    second-version-search-fix

--- src/components/Book.js
+++ src/components/Book.js
@@ -15,10 +15,15 @@ export default class Book extends Component {
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail || this.props.book.imageLinks.smallThumbnail || ""}")` }}></div>
+            <div className="book-cover"
+              style={{
+                width: 128,
+                height: 193,
+                backgroundImage: `url("${(this.props.book.imageLinks && this.props.book.imageLinks.thumbnail) || ""}")` }}></div>
             <div className="book-shelf-changer">
-              {/*Handling the correct default selected rack for the book*/}
-              <select value={this.props.book.shelf || "none"} onChange={(event) => {this.props.bookReloader(this.props.book, event.target.value)}}>
+              {/*Handling the correct default selected rack for the book and its future*/}
+              <select value={this.props.book.shelf || "none"}
+                      onChange={(event) => {this.props.bookReloader(this.props.book, event.target.value)}}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
@@ -27,8 +32,9 @@ export default class Book extends Component {
               </select>
             </div>
           </div>
-          <div className="book-title">{this.props.book.title}</div>
-          <div className="book-authors">{this.props.book.authors || "Author Not Found :( "}</div>
+          <div className="book-title">{this.props.book.title || "Title Disappeared O_O"}</div>
+          <div className="book-authors">
+          {(this.props.book.authors && this.props.book.authors[0]) || "No Author :("}</div>
         </div>
       </li>
     )
--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 //importing Link from library 'react-router-dom'
 import { Link } from 'react-router-dom'
-// import BooksAPI
+// import all from  BooksAPI
 import * as BooksAPI from '../../BooksAPI'
+//import the book components
+import Book from '../Book'
 
 export default class SearchPage extends Component {
-
-  //setting the initial state as Facebook React Docs
+  //setting the initial state in the Facebook React Docs "way"
   constructor(props) {
     super(props);
     this.state = {
       lib: [],
-      results: []
+      results: [],
+      query: ""
     }
   }
+
 //instantiate request into componentDidMount for laoding books
   componentDidMount() {
     BooksAPI.getAll()//calling the method from BookAPI
@@ -21,6 +24,34 @@ export default class SearchPage extends Component {
       //console.log(allBooks);
       this.setState({lib: allBooks })
     })
+  }
+
+  //updateQuery method to change the state
+  updateQuery = query => {
+		this.setState({ query: query }, this.submitSearch)
+	}
+
+
+  submitSearch() {
+    //handling empyty string or undefing with an empty result
+    if (this.state.query === "" || this.state.query === undefined) {
+      return this.setState({ results: [] })
+    }
+    BooksAPI.search(this.state.query.trim()).then(response => {
+      //handling error with undefined response or error
+      //we will give an empy book with no title no image no auhor
+      if (response === undefined || response.error ) {
+        return this.setState({ results: [""] });
+      } else {
+        response.forEach(bk => {
+          let fltr = this.state.lib.filter(Bk => Bk.id === bk.id);
+          if (fltr[0]) {
+            bk.shelf = fltr[0].shelf;
+          }
+        })
+        return this.setState({ results: response })
+      }
+    });
   }
 
   //This method is for updating the book
@@ -41,14 +72,26 @@ export default class SearchPage extends Component {
           <div className="search-books-bar">
             <Link to="/">
               {/*wrapping the button into Link to get the routing working*/}
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+              <button className="close-search">Close</button>
             </Link>
             <div className="search-books-input-wrapper">
-              <input type="text" placeholder="Search by title or author"/>
+              {/*Fix a type into the name error TypeError: _this3.updateQuery is not a function///
+                ===== adding value and monitoring the changes when we have changes ======
+                ======= call this.updateQuery passing in the new value ==========/////=/=//==/ */}
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                value={this.state.query}
+                onChange={event => this.updateQuery(event.target.value)}
+              />
             </div>
           </div>
           <div className="search-books-results">
-            <ol className="books-grid"></ol>
+            <ol className="books-grid">
+              {this.state.results.map((book, key) => (
+                <Book bookReloader={this.bookReloader} book={book} key={key} />
+              ))}
+            </ol>
           </div>
         </div>
       </div>
